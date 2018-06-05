5979a4a516230b28ea2f88427c8069aa9287df7c
Limlawan Dantai TIX17 <dantailimlawan@kamk.fi>
2018-06-05 11:17:57 +0300

    init project

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    private Transform lookAt;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - lookAt.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = lookAt.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 1f;
+    public float force = 10f;
+
+    private float horizontal_dir;
+    private float jump;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        horizontal_dir = Input.GetAxis("Horizontal");
+        jump = Input.GetAxis("Jump");
+
+        Vector3 movement = new Vector3(0.5f, 0f, -horizontal_dir);
+
+        rb.AddForce(movement * speed);
+        transform.position += movement * speed;
+    }
+}
